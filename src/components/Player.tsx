dd41183017d3c938cb520284af467cb72e83d5d3
harishtornado <harish20x12@gmail.com>
2023-09-21 23:55:50 +0530

    expo-AV setup

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,12 +1,58 @@
 import { View, Text, Image } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { styles } from "../styles/styles";
 import { tracks } from "../../assets/data/tracks";
 import { Ionicons } from "@expo/vector-icons";
 import { usePlayerContext } from "../providers/PlayerProvider";
+import { Audio } from "expo-av";
+import { Sound } from "expo-av/build/Audio";
+import { AVPlaybackStatus } from "expo-av/build/AV.types";
 
 const Player = () => {
+  const [sound, setSound] = useState<Sound>();
+  const [isPlaying, setIsPlaying] = useState(false);
   const { track } = usePlayerContext();
+
+  useEffect(() => {
+    playTrack();
+  }, [track]);
+
+  useEffect(() => {
+    return sound
+      ? () => {
+          sound.unloadAsync();
+        }
+      : undefined;
+  }, [sound]);
+
+  const playTrack = async () => {
+    if (sound) {
+      await sound.unloadAsync();
+    }
+    if (!track?.preview_url) return;
+    const { sound: newSound } = await Audio.Sound.createAsync({
+      uri: track.preview_url,
+    });
+    setSound(newSound);
+    newSound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+    await newSound.playAsync();
+  };
+
+  const onPlaybackStatusUpdate = (status: AVPlaybackStatus) => {
+    if (!status.isLoaded) return;
+
+    setIsPlaying(status.isPlaying);
+  };
+
+  const onPlayPause = async () => {
+    if (!sound) return;
+    if (isPlaying) {
+      await sound.pauseAsync();
+    } else {
+      await sound.playAsync();
+    }
+  };
+
   if (!track) {
     return null;
   }
@@ -29,7 +75,8 @@ const Player = () => {
         style={{ marginHorizontal: 10 }}
       />
       <Ionicons
-        name={"play"}
+        onPress={onPlayPause}
+        name={isPlaying ? "pause" : "play"}
         color={"white"}
         size={25}
         style={{ marginHorizontal: 10 }}
